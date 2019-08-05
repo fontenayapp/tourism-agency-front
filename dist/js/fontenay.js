d4dab2df855877a544509875abe8043c17578536
Federico Cardoso <cardoso@strands.com>
2019-08-05 15:42:27 -0300

    Test for fixing date error in iOS: Format not supported

--- dist/js/fontenay.js
+++ dist/js/fontenay.js
@@ -692,7 +692,7 @@ function _findProvider(id) {
 function _fixFormat(result) {
     var list = $.extend(true, [], result);
     list.forEach(function(e) {
-        e.date = _getFormatDateDDMMYYYY(new Date(e.date));
+        e.date = _getFormatDateDDMMYYYY(e.date);
         e.expenseText = e.is_expense ? "Si" : "No";
         e.method = _getMethodString(e.method);
         e.amountARS = (e.amount * e.exchange).toFixed("2");
@@ -751,7 +751,7 @@ function _fixSalesFormat(result) {
         elem.email = e.client.email;
         elem.clientname = e.client.name;
         elem.passport = e.client.passport_number;
-        elem.date = _getFormatDateDDMMYYYY(new Date(e.date));
+        elem.date = _getFormatDateDDMMYYYY(e.date);
         elem.seller = e.seller.first_name + ' ' + e.seller.last_name;
         elem.promoter = elem.promoter.first_name + ' ' + elem.promoter.last_name;
         elem.currency = e.currency || "N/A";
@@ -769,7 +769,7 @@ function _fixReportsFormat(result) {
         elem.saleid = e.sale_id;
         elem.userid = e.seller.user_id;
         elem.promoterid = elem.promoter.user_id;
-        elem.date = _getFormatDateDDMMYYYY(new Date(e.date));
+        elem.date = _getFormatDateDDMMYYYY(e.date);
         elem.seller = e.seller.first_name + " " + e.seller.last_name;
         elem.sellercommission = e.user_commission;
         elem.promoter = elem.promoter.first_name + " " + elem.promoter.last_name;
@@ -790,7 +790,7 @@ function _fixReportsPendingFormat(result) {
         var soldText = (e.adults !== 0 ? e.adults + " Ad. " : "") + (e.children !== 0 ? e.children + " Ni. " : "") + (e.babies !== 0 ? e.babies + " Be. " : "");
         elem.pendingid = e.sold_product_id;
         elem.providerid = e.product.provider.provider_id;
-        elem.date = _getFormatDateDDMMYYYY(new Date(e.date));
+        elem.date = _getFormatDateDDMMYYYY(e.date);
         elem.productname = e.product.name;
         elem.clientname = sale.client.name;
         elem.numbersold = soldText;
@@ -834,9 +834,10 @@ function _getExchangeRate(currency) {
     return exchRate;
 }
 
-function _getFormatDateDDMMYYYY(dat){
-    if(undefined === dat) {
-        dat = new Date();
+function _getFormatDateDDMMYYYY(stringDate){
+    var dat = new Date();
+    if(undefined !== stringDate) {
+        dat = new Date(stringDate.replace(/-/g, '/').replace(/T/g,' '));
     }
     var day = dat.getDate();
     var month = dat.getMonth()+1;
@@ -863,11 +864,12 @@ function _getFormatDateYYYYMMDDHHMMSS(dat){
     return dat.getFullYear()+"-"+month+"-"+day+" "+hours+":"+minutes+":"+seconds;
 }
 
-function _getFormatDateDDMMYYYYHHMM(dat){
-    if(undefined === dat) {
-        dat = new Date();
+function _getFormatDateDDMMYYYYHHMM(stringDate){
+    var dat = new Date();
+    if(undefined !== stringDate) {
+        dat = new Date(stringDate.replace(/-/g, '/').replace(/T/g,' '));
     }
-    var dateStr = _getFormatDateDDMMYYYY(dat);
+    var dateStr = _getFormatDateDDMMYYYY(stringDate);
     var hours = dat.getHours();
     var minutes = dat.getMinutes();
 
@@ -910,7 +912,7 @@ function _getLongCurrentDate(){
 function loadProdTableRow(product, tableBody) {
     var td1 = $(document.createElement("td")).text(product.product.provider.name);
     var td2 = $(document.createElement("td")).text(product.product.name);
-    var td3 = $(document.createElement("td")).text(_getFormatDateDDMMYYYY(new Date(product.date)));
+    var td3 = $(document.createElement("td")).text(_getFormatDateDDMMYYYY(product.date));
     var td4 = $(document.createElement("td")).text(product.transfer ? "Sí" : "No");
     var td5 = $(document.createElement("td")).text(product.adults);
     var td6 = $(document.createElement("td")).text(product.children);
